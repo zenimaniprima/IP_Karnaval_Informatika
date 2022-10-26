4bb8c7c4d759e694b5a06886f06fc549b21d3426
Dwi Suci Amelia <dwi.amelia@imaniprima.com>
2022-10-26 15:03:50 +0700

    :art: Validasi dan Connect db
    membuat validasi dan menyimpan data ke database

--- components/bukuTamu.vue
+++ components/bukuTamu.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="surface-card p-3 md:p-4 shadow-6 border-round w-full md:w-6 mx-6">
+        <Toast />
         <Card>
             <template #title>
                 <div class="text-center">
@@ -8,30 +9,44 @@
                 </div>
             </template>
             <template #content>
-                <label for="nama" class="block text-900 font-medium mb-2">Nama</label>
-                <InputText id="nama" type="text" class="w-full mb-3" v-model="data.nama" />
+                <Form>
+                <label for="nama" class="block text-900 font-medium mb-2">Nama<span style="color:red;">*</span></label>
+                <Field class="w-full mb-3" id="nama" name="nama" :rules="isRequired" v-model="data.nama" />
+                <ErrorMessage name="nama"><small style="color:red;">Nama harus diisi</small></ErrorMessage>
+                <!-- <InputText id="nama" type="text" class="w-full mb-3" v-model="data.nama" /> -->
 
-                <label for="nohp" class="block text-900 font-medium mb-2">No. HP</label>
-                <InputText id="nohp" type="text" class="w-full mb-3" v-model="data.noHp" />
+                <label for="nohp" class="block text-900 font-medium mb-2">No. HP<span style="color:red;">*</span></label>
+                <Field class="w-full mb-3" id="nohp" name="nohp" :rules="isRequired" v-model="data.noHp" />
+                <ErrorMessage name="nohp"><small style="color:red;">No HP harus diisi</small></ErrorMessage>
+                <!-- <InputText id="nohp" type="text" class="w-full mb-3" v-model="data.noHp" /> -->
 
                 <div class="formgrid grid">
                     <div class="field col">
-                        <label for="jurusan" class="block text-900 font-medium mb-2">Jurusan</label>
-                        <InputText id="jurusan" type="text" class="w-full mb-3" v-model="data.jurusan" />
+                        <label for="jurusan" class="block text-900 font-medium mb-2">Jurusan<span style="color:red;">*</span></label>
+                        <Field class="w-full mb-3" id="jurusan" name="jurusan" :rules="isRequired" v-model="data.jurusan" />
+                        <ErrorMessage name="jurusan"><small style="color:red;">Jurusan harus diisi</small></ErrorMessage>
+                        <!-- <InputText id="jurusan" type="text" class="w-full mb-3" v-model="data.jurusan" /> -->
                     </div>
                     <div class="field col">
-                        <label for="Angkatan" class="block text-900 font-medium mb-2">Angkatan</label>
-                        <InputText id="Angkatan" type="text" class="w-full mb-3" v-model="data.angkatan" />
+                        <label for="Angkatan" class="block text-900 font-medium mb-2">Angkatan<span style="color:red;">*</span></label>
+                        <Field class="w-full mb-3" id="Angkatan" name="Angkatan" :rules="isRequired" v-model="data.angkatan" />
+                        <ErrorMessage name="Angkatan"><small style="color:red;">Angkatan harus diisi</small></ErrorMessage>
+                        <!-- <InputText id="Angkatan" type="text" class="w-full mb-3" v-model="data.angkatan" /> -->
                     </div>
                 </div>
                 <Button label="Masuk" icon="pi pi-user" class="mt-3 p-button-warning" style="float:right"
                     @click="masuk" />
+                </Form>
             </template>
         </Card>
     </div>
 </template>
 
 <script setup>
+import axios from 'axios';
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
 
 const data = reactive({
     nama: "",
@@ -47,7 +62,24 @@ const isRequired = (value) => {
     return true;
 };
 const masuk = () => {
-    console.log(data)
+    try {
+        axios.post("http://localhost:3000/api/landing/guests", {
+            name: data.nama,
+            phone: data.noHp,
+            major: data.jurusan,
+            generation: data.angkatan,
+        }).then(() => {
+            toast.add({ severity: "info", summary: "Enjoy the Carnaval" });
+        })
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 </script>
+
+<style>
+.mb-3{
+    height: 35px;
+}
+</style>
--- plugins/primevue.js
+++ plugins/primevue.js
@@ -3,11 +3,15 @@ import PrimeVue from "primevue/config";
 import Button from "primevue/button";
 import InputText from 'primevue/inputtext';
 import Card from 'primevue/card';
+import Toast from "primevue/toast";
+import ToastService from 'primevue/toastservice';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
+    nuxtApp.vueApp.use(ToastService);
     nuxtApp.vueApp.component('Button', Button);
     nuxtApp.vueApp.component('InputText', InputText);
     nuxtApp.vueApp.component('Card', Card);
+    nuxtApp.vueApp.component('Toast', Toast);
     //other components that you need
 });
